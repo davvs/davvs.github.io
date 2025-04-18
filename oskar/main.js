ab11dae9d85d9f63a924eed16b8313d7236e7116
David Viklund <dviklund@blizzard.com>
2025-04-18 22:59:32 +0200

    wip

--- oskar/main.js
+++ oskar/main.js
@@ -3,11 +3,20 @@
 // a monster is a object that contains name, hp, and image
 
 class Monster {
-    constructor(id, hp) {
+    constructor(id) {
         this.id = id;
-        this.hp = hp;
+        this.hp = monsterDB.find(monster => monster.id === id).maxHp;
     }
 }
+
+
+// monster database
+let monsterDB = [
+    { id: 1, name: "Aquanid", image: "aquanid.png", maxHp: 90 },
+    { id: 2, name: "Floweron", image: "floweron.png", maxHp: 100 },
+    { id: 3, name: "Ancient Beast", image: "ancient_beast.png", maxHp: 110 }
+];
+
 
 class Backpack {
     constructor() {
@@ -29,34 +38,76 @@ class Backpack {
 // create a new inventory
 let backpack = new Backpack();
 
-// monster database
-let monsterDB = [
-    { id: 1, name: "Aquanid", image: "aquanid.png" },
-    { id: 2, name: "Floweron", image: "Bob.png" },
-    { id: 3, name: "Ancient Beast", image: "Bob.png" }
-];
-
-function choice(monsterId){
-    let monsterDef = monsterDB.find(monster => monster.id === monsterId);
-
-    let monster = new Monster(monsterId, 100);
-    backpack.addMonster(monster);
-    let monsterName = monsterDef.name;
-    alert("HEJ du tog " + monsterName)
-    updateInventory();
-}
-
 function updateInventory() {
     let inventory = document.getElementById("inventory");
-    inventory.innerHTML = ""; // Clear the inventory display
+    inventory.innerHTML = "<h2>Dina monster:</h2>"; // Clear the inventory display
 
     backpack.monsters.forEach(monster => {
         let monsterDiv = document.createElement("div");
         let monsterId = monster.id;
-        monsterImg = monsterDB.find(m => m.id === monsterId).image;
-        monsterName = monsterDB.find(m => m.id === monsterId).name;
-        monsterDiv.innerHTML = `<img src="${monsterImg}" alt="${monsterName}"><p>${monsterName}</p>`;
+        let monsterDef = monsterDB.find(m => m.id === monsterId);
+        let monsterImg = monsterDef.image;
+        let monsterName = monsterDef.name;
+        let hpMax = monsterDef.maxHp;
+        let hp = monster.hp;
+        monsterDiv.innerHTML = `<img src="${monsterImg}" alt="${monsterName}"><p>${monsterName} HP:${hp}/${hpMax}</p>`;
         inventory.appendChild(monsterDiv);
     });
 }
+
+function updateEncounter(encounterText) {
+    let encounter = document.getElementById("encounter");
+    encounter.innerHTML = encounterText;
+}
+
+function updateChoices(){
+    let choices = document.getElementById("choices");
+    choices.innerHTML = ""; // Clear the choices display
+
+    //generate two buttons
+    let button1 = document.createElement("button");
+    button1.innerHTML = "Mörka skogen";
+    button1.onclick = function() { enterDarkWoods(); };
+    choices.appendChild(button1);
+
+    let button2 = document.createElement("button");
+    button2.innerHTML = "Grottan";
+    // button1.onclick = function() { alert("TODO"); };
+    choices.appendChild(button2);
+}
+
+function updateChoicesDarkWoods(){
+    let choices = document.getElementById("choices");
+    choices.innerHTML = ""; // Clear the choices display
+
+    //generate two buttons
+    let button1 = document.createElement("button");
+    button1.innerHTML = "Undersöka lyktan";
+    // button1.onclick = function() { enterDarkWoods(); };
+    choices.appendChild(button1);
+
+    let button2 = document.createElement("button");
+    button2.innerHTML = "Springa tillbaka";
+    // button1.onclick = function() { alert("TODO"); };
+    choices.appendChild(button2);
+}
+
+function enterDarkWoods(){
+    updateEncounter("Du är i den mörka skogen. Du ser en pumpalykta på marken som lyser. Vad vill du göra?");
+    updateChoicesDarkWoods();
+}
+
+function choice(monsterId){
+    let monsterDef = monsterDB.find(monster => monster.id === monsterId);
+
+    let monster = new Monster(monsterId);
+    backpack.addMonster(monster);
+    let monsterName = monsterDef.name;
+    alert("HEJ du tog " + monsterName)
+    updateInventory();
+    updateEncounter("Vad vill du utforska nu?");
+    updateChoices();
+
+}
+
 
